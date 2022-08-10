d6ea7896d0d5307872827c996263574a7bfb7071
Guillaume Huysmans <ghuysmans99@gmail.com>
2022-08-10 03:52:39 +0200

    Parse Sys.argv in test

--- tyxml/main.ml
+++ tyxml/main.ml
@@ -1,29 +1,39 @@
 type t = {x: string option; y: bool} [@@deriving show]
 
-module Make (I : module type of Conformist_tyxml) = struct
-  open I
+open Conformist_tyxml
 
-  let direct =
-    let open Tyxml.Html in
-    let x, xs = render (optional (string ~meta:() "x")) in
-    let y, ys = render (bool "y" ~default:true) in
-    div [txt "enter x: "; x; txt " and y: "; y],
-    Conformist.(make Field.[xs; ys]) (fun x y -> {x; y})
+let direct =
+  let open Tyxml.Html in
+  let x, xs = render (optional (string ~meta:() "x")) in
+  let y, ys = render (bool "y" ~default:true) in
+  div [txt "enter x: "; x; txt " and y: "; y],
+  Conformist.(make Field.[xs; ys]) (fun x y -> {x; y})
 
-  (*
-  let monadic =
-    let open Tyxml.Html in
-    make (
-      let< x = get (string ~meta:() "x") in
-      let< y = get (bool "y" ~default:true) in
-      return (div [txt "enter x: "; x; txt " and y: "; y])
-    ) (fun x y -> {x; y})
-  *)
+(*
+let monadic =
+  let open Tyxml.Html in
+  make (
+    let< x = get (string ~meta:() "x") in
+    let< y = get (bool "y" ~default:true) in
+    return (div [txt "enter x: "; x; txt " and y: "; y])
+  ) (fun x y -> {x; y})
+*)
 
-  let run t =
-    let f, sch = t in
-    Format.printf "%a@." (Tyxml.Html.pp_elt ()) f;
-    match Conformist.decode sch ["x", ["1"]; "y", []] with
-    | Error _ -> prerr_endline "error"
-    | Ok t -> print_endline (show t)
-end
+let run t =
+  let f, sch = t in
+  Format.printf "%a@." (Tyxml.Html.pp_elt ()) f;
+  let params =
+    Array.to_list Sys.argv |>
+    List.tl |>
+    List.map (fun x ->
+      match String.split_on_char '=' x with
+      | [k] -> k, []
+      | [k; v] -> k, [v]
+      | _ -> failwith "invalid format, expected key=value"
+    )
+  in
+  match Conformist.decode sch params with
+  | Error _ -> prerr_endline "error"
+  | Ok t -> print_endline (show t)
+
+let () = run direct
